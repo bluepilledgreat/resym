f2da4e2e5f36d2fc86378b013a531a1f01fa8034
Matt <97983689+bluepilledgreat@users.noreply.github.com>
2026-06-04 17:28:03 +0100

    update primitive kind string representations

--- resym_core/src/pdb_types/primitive_types.rs
+++ resym_core/src/pdb_types/primitive_types.rs
@@ -91,10 +91,13 @@ fn primitive_kind_as_str_portable(
         pdb::PrimitiveKind::U8 => Ok("uint8_t"),
         pdb::PrimitiveKind::I16 | pdb::PrimitiveKind::Short => Ok("int16_t"),
         pdb::PrimitiveKind::U16 | pdb::PrimitiveKind::UShort => Ok("uint16_t"),
-        pdb::PrimitiveKind::I32 | pdb::PrimitiveKind::Long => Ok("int32_t"),
-        pdb::PrimitiveKind::U32 | pdb::PrimitiveKind::ULong => Ok("uint32_t"),
+        pdb::PrimitiveKind::I32 => Ok("int32_t"),
+        pdb::PrimitiveKind::U32 => Ok("uint32_t"),
         pdb::PrimitiveKind::I64 | pdb::PrimitiveKind::Quad => Ok("int64_t"),
         pdb::PrimitiveKind::U64 | pdb::PrimitiveKind::UQuad => Ok("uint64_t"),
+
+        pdb::PrimitiveKind::Long => Ok("long"),
+        pdb::PrimitiveKind::ULong => Ok("unsigned long"),
 
         pdb::PrimitiveKind::F32 => Ok("float"),
         pdb::PrimitiveKind::F64 => Ok("double"),
@@ -102,8 +105,7 @@ fn primitive_kind_as_str_portable(
         pdb::PrimitiveKind::Bool8 => Ok("bool"),
         pdb::PrimitiveKind::Bool32 => Ok("int32_t"),
 
-        // Microsoft-specific, usually implemented as "long"
-        pdb::PrimitiveKind::HRESULT => Ok("int32_t"),
+        pdb::PrimitiveKind::HRESULT => Ok("HRESULT"),
 
         // TODO: Seems valid for C++ method parameters. Are there other
         // cases of legitimate "NoType" occurences?
